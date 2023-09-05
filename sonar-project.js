b745cbdcb00ba4334fb4805192f9b929e0ce5ce6
vkingmaker <vkingmaker1@gmail.com>
2023-09-05 02:33:25 -0400

    update sonarqube configuration file to use authentication token

--- sonar-project.js
+++ sonar-project.js
@@ -3,8 +3,7 @@ const sonarqubeScanner = require('sonarqube-scanner');
        serverUrl: 'http://192.168.56.105:9000',
        options : {
        'sonar.sources': '.',
-	'sonar.login': 'admin',
-	'sonar.password':' '
+	'sonar.token': 'squ_3e00163a90578904402d6217a5d0a89eae43507a'
        //'sonar.inclusions' : '.' // Entry point of your code
        }
      }, () => {});
